d8fe9fc997070be8a91b4b08a688cfb4a8ece399
Hafiz-Al-Shams <hafizalshams1917@gmail.com>
2024-10-25 17:15:26 +0600

    banner button funtionality done

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import Banner from "./components/Banner"
 import Footer from "./components/Footer"
 import Navbar from "./components/Navbar"
@@ -5,14 +6,22 @@ import Newsletter from "./components/Newsletter"
 
 function App() {
 
+  const [balance, setBalance] = useState(0);
+
+  const handleAddBalance = () => {
+    setBalance(balance + 2500000);
+  }
+
+
+
   return (
     <>
       <div className="">
         <div className="w-11/12 mx-auto">
           {/* navbar */}
-          <Navbar></Navbar>
+          <Navbar balance={balance}></Navbar>
           {/* banner */}
-          <Banner></Banner>
+          <Banner handleAddBalance={handleAddBalance}></Banner>
 
           {/* newsletter */}
           <Newsletter></Newsletter>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,11 @@
 import logo from '../assets/logo.png'
 import coin from '../assets/coin.png'
 
-export default function Navbar() {
+export default function Navbar({ balance }) {
+
+
+
+
     return (
         <div className="flex justify-between items-center my-6">
             <div>
@@ -13,7 +17,7 @@ export default function Navbar() {
                 <p><a href="">Teams</a></p>
                 <p><a href="">Schedules</a></p>
                 <div className="flex justify-center items-center gap-1 rounded-xl font-semibold border-2 p-3">
-                    <p className='text-black'><span id="main-balance">10000000</span> Coin</p>
+                    <p className='text-black'><span id="main-balance">{balance}</span> Coin</p>
                     <img src={coin} alt="" className="w-2/12" />
                 </div>
             </div>
